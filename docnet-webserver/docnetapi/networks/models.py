008729d1c97344ed4c884a02df602af8813ae745
sinapad <malinoski.iuri@gmail.com>
2019-07-29 11:42:43 -0300

    Included some real tests for networks using tokens. On the tests was identifyed some inconsistences (=)), and was fixed (title was optional, where must be obligated)

--- docnet-webserver/docnetapi/networks/models.py
+++ docnet-webserver/docnetapi/networks/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-# from pygments.lexers import get_lexer_by_name
-# from pygments.formatters.html import HtmlFormatter
-# from pygments import highlight
-
 NETWORK_TYPES = [('LAN', 'Local area network'), ('MAN', 'Metropolitan area network'), ('WAN', 'Wide area network')]
 
 
 class Network(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    description = models.TextField()
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True, default='')
     enabled = models.BooleanField(default=True)
     type = models.CharField(choices=NETWORK_TYPES, default='python', max_length=100)
     owner = models.ForeignKey('auth.User', related_name='networks', on_delete=models.CASCADE)
--- docnet-webserver/docnetapi/networks/tests.py
+++ docnet-webserver/docnetapi/networks/tests.py
@@ -1,24 +1,63 @@
-# from django.urls import reverse
-# from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from rest_framework import status
 from rest_framework.test import APITestCase
-# from .models import Network
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+
+from .models import Network
 
 
-class DummyTests(APITestCase):
+class NetworkTests(APITestCase):
+
+    def setUp(self):
+
+        # Create a user and collect the token
+        self.user = User.objects.create_user(username='admin', email='admin@example.com', password='admin')
+        token = Token.objects.create(user=self.user)
+
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
     def test_dummy_test(self):
-        print("----------- test_dummy_test ----------- ")
+
+        print("\n\n----------- test_dummy_test ----------- ")
         self.assertTrue(True)
 
-'''
-class NetworkTests(APITestCase):
     def test_create_network(self):
 
+        print("\n\n----------- test_create_network ----------- ")
+
+        data = {
+            "title": "New Network",
+            "description": "A description"
+        }
+
         url = reverse('network-list')
-        data = {'title': 'New Network'}
+        # or
+        # url = '/networks/'
+
+        # response = self.client.post(reverse('network-list'), data, format='json')
         response = self.client.post(url, data, format='json')
-        print(response)
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Network.objects.count(), 1)
-        self.assertEqual(Network.objects.get().name, 'New Network')
-'''
+
+    def test_create_network_with_no_title(self):
+
+        print("\n\n----------- test_create_network_with_no_title ----------- ")
+
+        data = {
+            "title"
+            "description": "A description"
+        }
+
+        url = reverse('network-list')
+        # or
+        # url = '/networks/'
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Network.objects.count(), 0)
