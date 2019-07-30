97996940af0e37349d70658e9756723640c60119
sinapad <malinoski.iuri@gmail.com>
2019-07-30 19:44:07 -0300

    It was created the backend for update networks. The frontend was upgraded too.

--- docnet-webclient/app/modules/networks/networks.js
+++ docnet-webclient/app/modules/networks/networks.js
@@ -28,6 +28,7 @@ angular.module('myApp.networks', ['ngRoute'])
 		AuthenticationService.ClearCredentials();
 	}
 	
+	/** REST request */
     $scope.getNetworks = function(){
     	
 		console.log("NetworksCtrl createNetwork ...");
@@ -50,10 +51,10 @@ angular.module('myApp.networks', ['ngRoute'])
 		});		
 	}
     
+    /** REST request */
     $scope.createNetwork = function($cidr, $title, $description, $enabled){
     	
     		console.log("NetworksCtrl createNetwork ...");
-    		console.log($rootScope.tokenHeaderConfig);
     		
     		$http.post(
     			$rootScope.webServerBaseUrl+'/networks/', 
@@ -89,26 +90,59 @@ angular.module('myApp.networks', ['ngRoute'])
     		
     }
     
-    // Used to fill the modal for update network 
+    /** Used to fill the modal for update network */ 
     $scope.loadUpdateNetworkModal = function(network){
     		$scope.updateNetworkForm.$setPristine()
+    		
+    		/**
+    		The code below CANT be used, because it will create a reference, not a copy. If the user alter the fields (imput), it will update the principal object.
     		$scope.networkToUpdate = network;
+    			
+    		The proper way is to make a copy:
+    		$scope.networkToUpdate = angular.copy(network);
+    		*/ 
+    		$scope.networkToUpdate = angular.copy(network);
     }
     
-    $scope.disableSendUpdateNetworkButton = function(updateInputCidr, updateInputTitle, networkToUpdate){
-    		console.log(updateInputCidr+" "+ updateInputTitle +" "+  networkToUpdate);
-    		if((networkToUpdate.cidr!=updateInputCidr) || (networkToUpdate.title!=updateInputTitle)) {
-    			return false;
-    		}else{
-    			return true;
-    		}
-    		console.log("validateNetworkToUpdate");
+    /** REST request */
+    $scope.updateNetwork = function($networkToUpdate){
+    		console.log("NetworksCtrl updateNetwork ... ");
+    		console.log($networkToUpdate);
+    		
+		$http.put(
+			$rootScope.webServerBaseUrl+'/networks/'+$networkToUpdate.id+'/', 
+	    		{
+				cidr: $networkToUpdate.cidr,
+				title: $networkToUpdate.title,
+				description: $networkToUpdate.description,
+				enabled: $networkToUpdate.enabled
+	    		},
+	    		$rootScope.tokenHeaderConfig
+	    ).then(function successCallback(response) {
+			
+			console.log("NetworksCtrl updateNetwork success!");
+			console.log(response);
+			$scope.getNetworks();
+			bootbox.alert("Network updated!");		
+			
+		}, function errorCallback(response) {
+			
+			console.log("NetworksCtrl updateNetwork failed!");
+			console.log(response);
+			
+			if(response.status==401){ // Unauthorized, but why?
+				$scope.checkToken();
+			}else{
+				bootbox.alert("Failed to update the network!" );    				
+			}
+			
+		}).finally(function() {
+			$('#updateNetworkModal').modal('hide');
+		});	
+		
     }
     
-    $scope.updateNetwork = function($cidr, $title, $description, $enabled){
-    		console.log("NetworksCtrl updateNetwork TODO ...");
-    }
-    
+    /** REST request */
     $scope.deleteNetwork = function($networkId){
     		console.log("NetworksCtrl deleteNetwork("+$networkId+") ...");
     		
@@ -144,6 +178,7 @@ angular.module('myApp.networks', ['ngRoute'])
     		
     }
     
+    /** REST request */
     $scope.checkToken = function(){
     	
 		console.log("NetworksCtrl checkToken ...");
