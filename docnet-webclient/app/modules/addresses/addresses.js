7304c734e3ce0b6d04adf1f8242a16bde663729a
sinapad <malinoski.iuri@gmail.com>
2019-07-31 08:56:46 -0300

    Created the modal for create address. Only the frontend was made.

--- docnet-webclient/app/modules/addresses/addresses.js
+++ docnet-webclient/app/modules/addresses/addresses.js
@@ -14,12 +14,43 @@ angular.module('myApp.addresses', ['ngRoute'])
 	
 	console.log("AddressesCtrl start ...");
 	
+	$scope.addresses = null;
+	$scope.ipNote = "For example: if CIDR networks is 192.168.0.0/24, the values can be between 192.168.0.0 to 192.168.0.255";
+	
 	$scope.init = function(){
-		
+		$scope.getAddresses();
 	}
     
 	$scope.logout = function(){
 		AuthenticationService.ClearCredentials();
 	}
+	
+	/** REST request */
+    $scope.getAddresses = function(){
+    	
+		console.log("AddressesCtrl getAddresses ...");
+	
+		$http.get(
+			$rootScope.webServerBaseUrl+'/addresses/'										
+		).then(function successCallback(response) {
+			
+			console.log("AddressesCtrl getAddresses success!");
+			console.log(response);
+			$scope.addresses = response.data.results		
+			
+		}, function errorCallback(response) {
+			
+			console.log("AddressesCtrl getAddresses failed!");
+			console.log(response);			
+			
+		}).finally(function() {
+			// If needed
+		});		
+	}
+    
+    $scope.createAddress = function($inputIp, $inputTitle, $inputDescription, $network){
+    		console.log("AddressesCtrl createAddress ("+ $inputIp+", "+$inputTitle+", "+ $inputDescription+", "+ $network + ") ...");
+    		console.log("TODO");
+    }
     
 }]);
